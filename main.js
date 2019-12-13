66e1c0345b6a5a03e734dbdfb0175e90f253b4b2
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-13 15:01:03 +0100

    Creazione Files

---
+++ main.js
@@ -0,0 +1,14 @@
+// Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music che restituisce una lista di 10 dischi musicali.
+// Utilizzando handlebars, disegnare in pagina una card per ogni disco.
+// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
+// Nome repo: js-jq-ajax-api-musica
+
+
+// var template_html = $('#templateRow').html();
+// // compilo il template con la funzione specifica di Handlebars
+// var template_function = Handlebars.compile(template_html);
+// // creo l'oggetto con le proprietà che mi interesseranno
+// // do il mio oggetto in pasto alla funzione
+// var final = template_function(properties);
+// // lo appendo al container
+// $('.container').append(final);
