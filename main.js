e133602364e56472c8315536c23dbfc422543d17
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-13 15:44:13 +0100

    Esercizio e Bonus Terminati - esercizio molto semplice, l'avevamo fatto già questa mattina in classe con Sofia

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-
-
-// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
-
-// ho preso direttamente il layout d'esempio
+// Ho preso direttamente il layout d'esempio
 
 // strumenti utili alla creazione della funzione Handlebars
 var template_html = $("#template").html();
@@ -10,7 +6,6 @@ var template_function = Handlebars.compile(template_html);
 
 // Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music che restituisce una lista di 10 dischi musicali.
 // Utilizzando handlebars, disegnare in pagina una card per ogni disco.
-
 $.ajax({
     'url': 'https://flynn.boolean.careers/exercises/api/array/music',
     'method': 'GET',
@@ -37,4 +32,32 @@ $.ajax({
     'error': function(){
         alert('error');
     }
+});
+
+// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
+
+// come visto stamattina in classe, change si adatta bene al select
+// e al cambio del value
+$('#selector').change(function(){
+    // mi porto dietro il valore che ho selezionato
+    var valore = $(this).val();
+    // se il valore è una stringa vuota, quindi il primo elemento nel nostro html
+    if (valore == '') {
+        // mostra tutti i cd
+        $('.cd').fadeIn();
+    } else{
+        // selezione ogni cd presente
+        $('.cd').each(function(){
+            // mi porto dietro il data-genre di ogni cd per confrontarlo
+            var data_cd_corrente = $(this).attr('data-genre');
+            // se il val corrisponde (il lowercase qui è fondamentale)
+            if (data_cd_corrente.toLowerCase() == valore.toLowerCase()) {
+                // mostralo
+                $(this).fadeIn();
+            } else {
+                // altrimenti nascondilo
+                $(this).fadeOut();
+            }
+        });
+    };
 });
