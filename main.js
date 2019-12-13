477a0da9356d2f088256fbaa462d72c8a52199dd
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-12-13 15:29:07 +0100

    Prima parte con Handlebars e chiamata Ajx terminata, manca il bonus

--- main.js
+++ main.js
@@ -1,14 +1,40 @@
+
+
+// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
+
+// ho preso direttamente il layout d'esempio
+
+// strumenti utili alla creazione della funzione Handlebars
+var template_html = $("#template").html();
+var template_function = Handlebars.compile(template_html);
+
 // Eseguire una chiamata ajax all'api di boolean: https://flynn.boolean.careers/exercises/api/array/music che restituisce una lista di 10 dischi musicali.
 // Utilizzando handlebars, disegnare in pagina una card per ogni disco.
-// BONUS: aggiungere una tendina con i generi: pop, rock, metal e jazz, che permette all'utente di filtrare i dischi visualizzati in base al genere selezionato.
-// Nome repo: js-jq-ajax-api-musica
 
-
-// var template_html = $('#templateRow').html();
-// // compilo il template con la funzione specifica di Handlebars
-// var template_function = Handlebars.compile(template_html);
-// // creo l'oggetto con le proprietà che mi interesseranno
-// // do il mio oggetto in pasto alla funzione
-// var final = template_function(properties);
-// // lo appendo al container
-// $('.container').append(final);
+$.ajax({
+    'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+    'method': 'GET',
+    'success': function(data){
+        var array = data.response;
+        // ciclo for per scorrere l'array
+        for (var i = 0; i < array.length; i++) {
+            // ad ogni passaggio, analizzo un singolo oggetto
+            var disco = array[i];
+            // creo un oggetto con tutti i valori che mi interesseranno
+            // che darò poi in pasto alla funzione Handlebars
+            var properties = {
+                'cover': disco.poster,
+                'author': disco.author,
+                'title': disco.title,
+                'year': disco.year,
+                'genre': disco.genre
+            };
+            var final = template_function(properties);
+            // lo appendo al container
+            $('.cds-container.container').append(final);
+        }
+    },
+    'error': function(){
+        alert('error');
+    }
+});
